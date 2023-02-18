90a81a0c349b4964ade70772c7e059ecb8fb36b0
alndra7 <al.go9871@gmail.com>
2023-02-18 16:16:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log(x);
+//ПРАКТИЧЕСКИЕ ЗАДАНИЯ
+
+console.log (40000+22000);
+
+console.log ("Владивосток " + 2000);
+
+console.log (128+254+137+201);
+
+console.log (2**5);
+
+console.log ("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+const greetings = "Привет"; 
+const name = "Кот"; 
+console.log (greetings + ', ' + name);
+
+let time;
+time = 34;
+console.log ('Старт поездки. Осталось минут: ' + time);
+time = 19;
+console.log ('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = 9;
+console.log ('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log ('Вы приехали. Добро пожаловать в Москву');
+
+let fahrenheit;
+fahrenheit = 451;
+let celsius;
+celsius = (fahrenheit-32)/1.8;
+console.log (fahrenheit + ' градуса по Фаренгейту — это ' + celsius + ' градуса по Цельсию.');
